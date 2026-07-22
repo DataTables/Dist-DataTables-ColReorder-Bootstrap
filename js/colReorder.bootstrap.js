f6c6fef579508656f2dcf4bc5dd9493173a3e725
dtbuild <dtbuild@rigil>
2026-07-22 13:35:30 +0000

    a5f4bf12f9e29eaa65543175ca82252940d83634 Release 3.0.0
    Sync to source repo @a5f4bf12f9e29eaa65543175ca82252940d83634

--- js/colReorder.bootstrap.js
+++ js/colReorder.bootstrap.js
@@ -1,4 +1,4 @@
-/*! ColReorder Bootstrap 3 styling 3.0.0-beta.1 for DataTables
+/*! ColReorder Bootstrap 3 styling 3.0.0 for DataTables
  * Copyright (c) SpryMedia Ltd - datatables.net/license
  */
 
